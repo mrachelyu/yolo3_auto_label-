b95b26e0be56313b6d780e2427fae442fc1d5a1b
RachelYu <m.rachel.yu@gmail.com>
2019-07-01 02:49:36 +0800

    add new index

--- list.py
+++ list.py
@@ -8,7 +8,7 @@ list_a = []
 list_b = list()             
 list_c = [1,2.0,"3.0","a"] #型態不需一致
 
-
+'''
 print("type(list_c):", type(list_c))
 print("list_c:", list_c)
 
@@ -21,3 +21,82 @@ print("list_a:", list_a)
 tuple_a = (1, 'a', 3.14)
 list_b = list(tuple_a)
 print("list_b:", list_b)
+'''
+
+list_d = [1,9,9,7,9,5]
+list_d.append(0,1)             #append a number 0
+print(list_d)                       #a = [1,1,4,5,1,4,0]
+
+list_d = [1,9,9,7,9,5]         
+list_d.insert(1,0)           #append a number 0 after 1
+
+
+print(list_d)               	     #a = [1,0,1,4,5,1,4]
+
+
+
+
+
+
+
+list_d = [1,1,4,5,1,4]
+
+
+list_d.remove(1)             #remove 1
+
+
+print(list_d)
+
+
+print("")
+
+
+#--------------------------------------------------------------------------
+'''
+
+a = [1,1,4,5,1,4]
+
+
+print(a[1])             #1
+
+
+print(a[-1])            #4
+
+
+print(a[0:3])           #[1,1,4]
+
+
+print(a[5:])            #[4]
+
+
+print(a[-3:])           #[5,1,4]
+
+
+print("")
+
+'''
+#--------------------------------------------------------------------------
+
+'''
+print(a.index(4))       #2
+
+
+print(a.count(1))       #3
+
+
+print("")
+
+'''
+#--------------------------------------------------------------------------
+'''
+
+a.sort()                #sort from small to large
+
+
+print(a)
+
+
+a.sort(reverse=True)    #sort from large to small
+
+'''
+
