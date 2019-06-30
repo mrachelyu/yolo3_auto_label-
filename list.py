f4942621c1380175306f07077301fee57cc5a6ef
RachelYu <m.rachel.yu@gmail.com>
2019-07-01 03:06:05 +0800

    add new index

--- list.py
+++ list.py
@@ -6,7 +6,7 @@
 
 list_a = []                
 list_b = list()             
-list_c = [1,2.0,"3.0","a"] #型態不需一致
+list_c = [1,2.0,"3.0","a"]	#型態不需一致
 
 '''
 print("type(list_c):", type(list_c))
@@ -24,79 +24,51 @@ print("list_b:", list_b)
 '''
 
 list_d = [1,9,9,7,9,5]
-list_d.append(0,1)             #append a number 0
-print(list_d)                       #a = [1,1,4,5,1,4,0]
+list_d.append(0)	#在串列最後面加0
+
+print(list_d)	#a = [1,9,9,7,9,5,0]
 
 list_d = [1,9,9,7,9,5]         
-list_d.insert(1,0)           #append a number 0 after 1
+list_d.insert(1,0)	#在1後面加0
 
+print(list_d)	#a = [1,0,9,9,7,9,5]
 
-print(list_d)               	     #a = [1,0,1,4,5,1,4]
-
-
-
-
-
-
-
-list_d = [1,1,4,5,1,4]
-
-
-list_d.remove(1)             #remove 1
-
+list_d = [1,9,1,9,7,9,5] 
+list_d.remove(1)             #移除坐左邊數過來第一個1
 
 print(list_d)
+print("")
+
+
+#--------------------------------------------------------------------------
+
+
+a =  [1,9,9,7,9,5]
+
+
+print(a[1])             #9
+
+
+print(a[-1])            #5
+
+
+print(a[0:3])           #[1,9,9]
+
+
+print(a[5:])            #[5]
+
+
+print(a[-3:])           #[7,9,5]  從-3往-1
 
 
 print("")
 
 
 #--------------------------------------------------------------------------
-'''
 
-a = [1,1,4,5,1,4]
-
-
-print(a[1])             #1
-
-
-print(a[-1])            #4
-
-
-print(a[0:3])           #[1,1,4]
-
-
-print(a[5:])            #[4]
-
-
-print(a[-3:])           #[5,1,4]
-
-
+print(a.index(9))       #1 括號中的數字位置
+print(a.count(9))       #3括號中的數字出現次數
 print("")
 
-'''
-#--------------------------------------------------------------------------
 
-'''
-print(a.index(4))       #2
-
-
-print(a.count(1))       #3
-
-
-print("")
-
-'''
-#--------------------------------------------------------------------------
-'''
-
-a.sort()                #sort from small to large
-
-
-print(a)
-
-
-a.sort(reverse=True)    #sort from large to small
-
-'''
 
